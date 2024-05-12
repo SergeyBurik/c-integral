c090038656d1433e16b50584ca767984bc711abe
Sergey Burik <sergioburik@gmail.com>
2024-05-12 23:45:18 +0300

    Update func.asm

--- func.asm
+++ func.asm
@@ -6,9 +6,8 @@ func1:
     mov ebp, esp        ; установка базового указателя
     finit
 	fld qword [ebp + 8] ; загрузка x в регистр ST0
-	fld qword [ebp + 8] ; загрузка x в регистр ST0
+	fld st0 ; загрузка x в регистр ST1
 
-    ; fld st1             ; копирование x из ST0 в ST1
     fmul                ; x*x
     fmul                ; (x*x)*x
     mov esp, ebp        ; восстановление указателя стека
