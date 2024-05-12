8cdf7dd76ab8540502e9ab9d59ac459c56ed70cd
Sergey Burik <sergioburik@gmail.com>
2024-05-12 23:17:13 +0300

    Update func.asm

--- func.asm
+++ func.asm
@@ -5,13 +5,18 @@ func1:
     push ebp
     mov ebp, esp
     finit
-    fld qword [ebp + 8]
-    sub esp, 8
+    fld qword [ebp + 8] ; load x
+    sub esp, 16
     mov dword [esp], -1
     fild dword [esp]
-    fmulp
+    fmulp ; x = -x
+
     fstp qword [esp]
-    call exp
-    add esp, 8
+
+	mov dword [esp + 8], 2
+    fild dword [esp + 8]
+    
+	call exp
+    add esp, 16
     pop ebp
     ret
