4e1f53e286b518bd380c05b76b2cecfedfe931b8
Sergey Burik <sergioburik@gmail.com>
2024-05-13 15:21:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -37,8 +37,8 @@ double integral(double (*f)(double), double a, double b, double eps2) {
     int n = 5;
     double h = 1;
 
-    while ((1 / 3) * fabs(prevRes - res) < eps2) {
-        printf("->>%lf %lf %lf\n", res, prevRes, (1 / 3) * fabs(prevRes - res));
+    while ((1 / 3) * fabs(prevRes - res) <= eps2) {
+        printf("->>%lf %lf %lf %lf\n", res, prevRes, (1 / 3) * fabs(prevRes - res), eps2);
         prevRes = res;
         h = (b - a) / n;
         res = (f(a) - f(b)) / 2;
